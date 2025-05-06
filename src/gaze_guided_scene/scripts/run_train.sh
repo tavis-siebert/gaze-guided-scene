18fbd42b70bf82bb1147bc61cfbe7665a508dc22
Timon Fercho <ferchoti@gmail.com>
2025-05-07 01:22:33 +0200

    chore(scripts): simplify run train script

--- src/gaze_guided_scene/scripts/run_train.sh
+++ src/gaze_guided_scene/scripts/run_train.sh
@@ -4,4 +4,4 @@
 #SBATCH --output=logs/train.out
 #SBATCH --time=12:00:00
 
-uv run src/gaze_guided_scene/main.py train --device gpu --task future_actions
+./run.sh train --device gpu --task future_actions
