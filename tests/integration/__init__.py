21ffe1b93e0fa3814c27672f49bf39bd5637cf9b
Timon Fercho <ferchoti@gmail.com>
2025-05-09 15:48:45 +0200

    test: simplify test dir structure

--- tests/integration/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Integration tests package for gaze-guided-scene-graph.
-
-These tests verify that different components work together correctly.
-""" 
--- tests/resources/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Test resources package for gaze-guided-scene-graph.
-
-This package contains shared fixtures, mock objects, and test data.
-""" 
