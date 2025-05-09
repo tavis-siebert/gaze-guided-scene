26e7dd9d7e7ad607a2fa707da0a24c359acd687e
Timon Fercho <ferchoti@gmail.com>
2025-05-09 22:53:51 +0200

    fix(models): drop danling reference

--- src/gazegraph/models/__init__.py
+++ src/gazegraph/models/__init__.py
@@ -7,12 +7,13 @@ object detection (CLIP, YOLO-World) used in the scene graph construction.
 
 from gazegraph.models.sift import SIFT
 from gazegraph.models.clip import ClipModel
-from gazegraph.models.yolo_world import YOLOWorldModel
+from gazegraph.models.yolo_world_model import YOLOWorldModel
 from gazegraph.models.yolo_world_ultralytics import YOLOWorldUltralyticsModel
-
+from gazegraph.models.yolo_world_onnx import YOLOWorldOnnxModel
 __all__ = [
     'SIFT',
     'ClipModel',
     'YOLOWorldModel',
-    'YOLOWorldUltralyticsModel'
+    'YOLOWorldUltralyticsModel',
+    'YOLOWorldOnnxModel'
 ] 
